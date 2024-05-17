cb198b051033b64f9685e6ec81c6bc1a21caf63e
dongjin0521 <dongjin0521@gmail.com>
2024-05-17 23:18:39 +0900

    jsp 추가, java controller 추가

--- src/main/java/software/softwareEngineering/controller/TestController.java
+++ src/main/java/software/softwareEngineering/controller/TestController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        System.out.println("test...");
-        return "index";
+    @GetMapping("/index")
+    public String index() {
+        System.out.println("index...");
+        return "/index";
     }
 }
