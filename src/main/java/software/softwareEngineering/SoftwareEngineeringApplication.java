a66911e2d7229904f05d0b1c638f9fbd0838a1d2
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-05-17 17:49:51 +0900

    first commit

---
+++ src/main/java/software/softwareEngineering/SoftwareEngineeringApplication.java
@@ -0,0 +1,13 @@
+package software.softwareEngineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineeringApplication.class, args);
+	}
+
+}
