06deb6bea571c495986311f86a9fdadab461f128
dongjin0521 <dongjin0521@gmail.com>
2024-06-22 19:31:54 +0900

    pointing table 추가로 관련 함수 추가

--- src/main/java/software/softwareEngineering/repository/SongRepository.java
+++ src/main/java/software/softwareEngineering/repository/SongRepository.java
@@ -13,6 +13,16 @@ public interface SongRepository extends JpaRepository<Song, Long> {
     @Query(value = "SELECT * FROM song", nativeQuery = true)
     List<Song> getAllSongs();
 
+    //playlist id 를 넘기면 포함된 모든 song 정보를 반환.
+    @Query(value = "select s.* from song as s left join pointing as p on s.id = p.song_id where p.playlist_id = :id", nativeQuery = true)
+    List<Song> getSongsByPlaylistId(@Param("id") Long id);
+
+    @Query(value = "select p.* from pointing as p where playlist_id =  :id", nativeQuery = true)
+    List<Song> getPointingByPlaylistId(@Param("id") Long id);
+
+    @Query(value = "UPDATE seSpotify.pointing SET like_yn = :yn where playlist_id = :id", nativeQuery = true)
+    void updatePointingByPlaylistId(@Param("id") Long id, @Param("id") String yn);
+
     @Query("select s from Song s where s.mode = 'Major' and s.playlist.id is null")
     List<Song> getExercise(Pageable pageable);
     @Query("select s from Song s where s.mode = 'Major' and s.playlist.id is null")
